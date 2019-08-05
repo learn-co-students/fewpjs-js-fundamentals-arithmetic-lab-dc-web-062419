a2f1a7d4c0a49b3d005a3a9f40b54ca571f04bd3
ruggz13 <nicolasruggieri7@gmail.com>
2019-08-05 13:09:07 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+
+let multiply
+let num1 = 31
+let num2 = 2
+multiply = num1 * num2
+
+let random = Math.random();
+
+let mod
+mod = 16%6
+
+let max
+max = Math.max(1,2,3,20);
