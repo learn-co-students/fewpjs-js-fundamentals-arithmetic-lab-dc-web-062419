348532b502e14a33f117bf877496d853ef726bbb
SkaMateria <skamateria@gmail.com>
2019-08-05 15:20:59 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2
+let num2 = 31
 
-// Write your code here
+var multiply = (num1 * num2);
+
+var random = Math.floor(Math.random() * 10) + 1;
+
+// set to an equation whose remainder is equal to 4
+var mod = 32 % 14
+
+// that will return 20 as the higest number in the set
+var max = Math.max(1, 4, 6, 8, 20)
