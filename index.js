a104fba46aaff62b809dd2ef680603307ecdf4ec
Luke O'Malley <Lukeomalley95@gmail.com>
2019-08-05 12:36:37 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
+const num1 = 1;
+const num2 = 62;
 
-// Write your code here
+const multiply = num1 * num2;
+
+const random = Math.random();
+
+const mod = 29 % 5;
+
+const max = Math.max(1, 2, 3, 4, 20);
